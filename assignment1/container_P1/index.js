25dac029ab1843872219bbefa14e6c4a89c5571d
alensden2 <alensanjohn1999@gmail.com>
2023-05-12 18:22:45 -0300

    Container 1 setup

--- assignment1/container_P1/index.js
+++ assignment1/container_P1/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const middleWare = require("./middleware.js")
+const {checkIfFileExists} = require("./middleware.js")
 
 const app = express()
 app.use(express.json());
 
-app.post("/calculate", middleWare.checkIfFileExists)
+app.post("/calculate", checkIfFileExists)
 
 app.listen(6000, () => {
     console.log("container_P1 running @ 6000")
--- assignment1/container_P1/middleware.js
+++ assignment1/container_P1/middleware.js
@@ -1,18 +1,29 @@
 const fs = require("fs");
 
-
-
-export const checkIfFileExists = (req,res) => {
-    const filePath = req.body.file;
+const checkIfFileExists = (req, res) => {
+  const filePath = req.body.file;
+  if (filePath === null) {
+    res.status(400).json({
+      file: null,
+      error: "Invalid JSON input.",
+    });
+  } else {
     /** file exists logic */
     fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-          console.error(`${filePath} does not exist`);
-          res.status(200).json({"state":"success"})
-          return;
-        }
-      
-        console.log(`${filePath} exists`);
-        res.status(400).json({"state":"fail"})
-      });
-}
+      if (err) {
+        res.status(200).json({
+          file: "file.dat",
+          error: "File not found.",
+        });
+        return;
+      }
+
+      console.log(`${filePath} exists`);
+      res.status(400).json({ state: "sending to next container" });
+    });
+  }
+};
+
+module.exports = {
+  checkIfFileExists,
+};
